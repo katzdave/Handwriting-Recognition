bc34214416f02e01d954f1717b75bf80b86066e8
katzdave <katzdave@yahoo.com>
2014-04-28 02:20:25 -0400

    new main files

---
+++ spellchecker/MainStats.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package spellchecker;
+
+/**
+ *
+ * @author David
+ */
+public class MainStats {  
+  static void main(String[] args){
+    TestDataParser tdp = new TestDataParser();
+    TestData td = tdp.GetTestData();
+    DictionaryAndFreqs df = new DictionaryAndFreqs();
+    Spellchecker sc = new Spellchecker(df);
+    
+    Statistics s = new Statistics(sc, td);
+//    for(Integer i : s.GetCorpusCounts()){
+//      System.out.println(i);
+//    }
+//    System.out.println();
+//    for(Integer i : s.GetCorpusFrequencies()){
+//      System.out.println(i);
+//    }
+    
+    //    Double[] d = s.GetStatsBySize(1000, 1, false, true);
+//    for(int i=1; i<=15; i++){
+//      System.out.println(i + "\t" + d[i]);
+//    }
+    
+    System.out.println(s.GetOverallStats(1000, 2, true, true));
+  }
+}
